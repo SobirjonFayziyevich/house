b8e6697479330f5199eb03c3bd5cd2943a858697
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-10-22 21:50:20 +0900

    feat: dotenv & project configuration

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 
-
+const dotenv = require("dotenv");
+dotenv.config();
 
 const http = require("http");
 const mongodb = require("mongodb");
-
-let db;
-const connectionString =
-    "mongodb+srv://pirmatovsobir23:KnSHlq9VR0dnUTzc@cluster0.kscois6.mongodb.net/house";  // mongodb URL ni quydik.
+const connectionString = process.env.MONGO_URL;   //MONGO_URL bizga kuchirib beradi.
+    // "mongodb+srv://pirmatovsobir23:KnSHlq9VR0dnUTzc@cluster0.kscois6.mongodb.net/house";  // .env papkasiga utkazib quydik hamma kurolmasligi uchun.
 mongodb.connect(                              // mongodbning connect methodidan foy
     connectionString,
     {
@@ -21,8 +20,9 @@ mongodb.connect(                              // mongodbning connect methodidan 
 
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 3000;
-            server.listen(PORT, function () {
+            let PORT = process.env. PORT || 3000;  //PORTni tekshirsin va PORTda malumot bulsa,
+            // usha malumotni saqlasin,bulmsasa 3000ni quysin.
+            server.listen(PORT, function () {       //serverni tanlangan PORTda ishga tushirib beradi.
                 console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
                 );
             });
