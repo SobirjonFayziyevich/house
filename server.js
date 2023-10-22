6c8b1d3b9630b55c60a75734970018b387662450
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-10-22 19:28:26 +0900

    feat: create project of house

---
+++ server.js
@@ -0,0 +1,31 @@
+
+
+
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+    "mongodb+srv://pirmatovsobir23:KnSHlq9VR0dnUTzc@cluster0.kscois6.mongodb.net/house";  // mongodb URL ni quydik.
+mongodb.connect(                              // mongodbning connect methodidan foy
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+             module.exports = client;
+
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
+
+    });
